b8849e57ac00fce930d0d0256c4092c707df86a9
Jason Yang <jasonyang96@ucla.edu>
2016-04-07 13:12:08 -0700

    setting command line arguments

--- web-server.cpp
+++ web-server.cpp
@@ -2,19 +2,65 @@
 #include <string>
 #include <thread>
 #include <iostream>
+#include <vector>
+#include <sys/types.h>
+#include <netdb.h>
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-	HttpRequest req;
-	req.setMethod("GET");
-	req.setUrl("/");
-	req.setHost("www.google.com");
-	req.setUserAgent("Mozilla/5.0");
-	req.setConnection("close");
+	// HttpRequest req;
+	// req.setMethod("GET");
+	// req.setUrl("/");
+	// req.setHost("www.google.com");
+	// req.setUserAgent("Mozilla/5.0");
+	// req.setConnection("close");
 
-	string requestString = req.createRequestString;
+	// string requestString = req.createRequestString();
 
-	cout << requestString << endl;
+	// cout << requestString << endl;
+
+	// TODO: DEBUG SEGMENTATION FAULT
+	const char DEFPORT[5] = "4000";
+	const char DEFDIR[2] = ".";
+
+	char *node = nullptr;
+	char *service;
+	char *directory;
+
+	// setting host name
+	if (argv[1]) {
+		size_t len = strlen(argv[1]);
+		node = (char *)(malloc(len * sizeof(char) + 1));
+		strncpy(node, argv[1], len);
+	}
+
+	// setting port number
+	if (argv[2]) {
+		size_t len = strlen(argv[2]);
+		service = (char *)(malloc(len * sizeof(char) + 1));
+		strncpy(service, argv[2], len);
+	}
+	else {
+		size_t len = strlen(DEFPORT);
+		service = (char *)(malloc(len * sizeof(char) + 1));
+		strncpy(service, DEFPORT, len);
+	}
+
+	// set file-directory
+	if (argv[2]) {
+		size_t len = strlen(argv[3]);
+		directory = (char *)(malloc(len * sizeof(char) + 1));
+		strncpy(directory, argv[3], len);
+	}
+	else {
+		size_t len = strlen(DEFDIR);
+		directory = (char *)(malloc(len * sizeof(char) + 1));
+		strncpy(directory, DEFDIR, len);
+	}
+
+	cout << node << endl;
+	cout << service << endl;
+	cout << directory << endl;
 }
