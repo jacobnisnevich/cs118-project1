ee98891d66b6473b22e8b8401a5b53f983b8cd1b
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2016-04-09 19:02:31 -0700

    changed encode and consume to use string

--- http-request.h
+++ http-request.h
@@ -23,10 +23,10 @@ public:
     std::string create_request_string();
 
     // Encode HttpRequest into string of bytes
-    std::vector<uint8_t> encode();
+    std::string encode();
 
     // Decode string of bytes to HttpRequest
-    void consume(std::vector<uint8_t> wire);
+    void consume(std::string wire);
 private:
     std::string m_method;
     std::string m_url;
