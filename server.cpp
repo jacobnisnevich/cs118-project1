9b92a8622ee287d710f58119de01b8557c7ac108
Jason Yang <jasonyang96@ucla.edu>
2016-04-09 17:52:31 -0700

    added check for res linked list

--- server.cpp
+++ server.cpp
@@ -19,11 +19,11 @@ Server::Server(char* host, char* port, char* dir)
     int status;
 
     memset(&hints, 0, sizeof(hints));
-
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
+    // set up socket calls
     status = getaddrinfo(host, port, &hints, &res);
     if (status != 0)
     {
@@ -31,16 +31,34 @@ Server::Server(char* host, char* port, char* dir)
         exit(1);
     }
 
-    m_sockfd = socket(res->ai_family, res->ai_socktype, 0);
-    process_error(m_sockfd, "socket");
+    // find socket to bind to
+    auto i = res;
+    for (; i != NULL; i = i->ai_next)
+    {
+        status = setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int)) 
+        if (status == -1) continue;
 
-    status = bind(m_sockfd, res->ai_addr, res->ai_addrlen);
-    process_error(status, "bind");
+        sockfd = socket(res->ai_family, res->ai_socktype, 0);
+        if (sockfd == -1) continue;
 
-    status = listen(m_sockfd, 10);
-    process_error(status, "listen");
+        status = bind(sockfd, res->ai_addr, res->ai_addrlen);
+        if (status == -1) continue;
+
+        break;
+    }
 
     freeaddrinfo(res);
+
+    // check if reached end of linked list
+    if (i == NULL)
+    {
+        cout << "could not bind to any sockets"
+        exit(1);
+    }
+
+    // start listening
+    status = listen(sockfd, 10);
+    process_error(status, "listen");
 }
 
 bool Server::accept_connections()
@@ -49,7 +67,7 @@ bool Server::accept_connections()
     {
         struct sockaddr_storage accepted_addr;
         socklen_t addr_size = sizeof(accepted_addr);
-        int new_fd = accept(m_sockfd, (struct sockaddr *) &accepted_addr, &addr_size);
+        int new_fd = accept(sockfd, (struct sockaddr *) &accepted_addr, &addr_size);
         if (new_fd > 0)
         {
             cout << "accepted" << endl;
