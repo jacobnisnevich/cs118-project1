249bc89fff6fd7c4c09189688a5a80f7d6833283
Jason Yang <jasonyang96@ucla.edu>
2016-04-21 12:00:00 -0700

    made client receive response

--- client.cpp
+++ client.cpp
@@ -1,16 +1,19 @@
 #include "client.h"
 #include "http-request.h"
 #include <string>
+#include <cstring>
 #include <errno.h>
 #include <netdb.h>
 #include <sys/types.h>
 #include <iostream>
+#include <unistd.h>
 
-Client::Client(std::string host, std::string port, std::string file_path)
+using namespace std;
+
+Client::Client(string host, string port, string file_path)
 {
     struct addrinfo hints;
     struct addrinfo *res;
-    int m_sockfd;
     int status;
 
     memset(&hints, 0, sizeof(hints));
@@ -21,14 +24,15 @@ Client::Client(std::string host, std::string port, std::string file_path)
     status = getaddrinfo(host.c_str(), port.c_str(), &hints, &res);
     if (status != 0)
     {
-        std::cout << "getaddrinfo error: " << gai_strerror(status) << std::endl;
+        cout << "getaddrinfo error: " << gai_strerror(status) << endl;
         exit(1);
     }
 
-    m_sockfd = socket(res->ai_family, res->ai_socktype, 0);
-    process_error(m_sockfd, "socket");
+    // TODO: loop through res linked list
+    int sock_fd = socket(res->ai_family, res->ai_socktype, 0);
+    process_error(sock_fd, "socket");
 
-    status = connect(m_sockfd, res->ai_addr, res->ai_addrlen);
+    status = connect(sock_fd, res->ai_addr, res->ai_addrlen);
     process_error(status, "connect");
 
     HttpRequest req;
@@ -36,11 +40,51 @@ Client::Client(std::string host, std::string port, std::string file_path)
     req.set_url(file_path);
     req.set_version("1.0");
 
-    std::string request = req.encode();
-    send(m_sockfd, request.c_str(), request.size(), 0);
+    string request = req.encode();
+    send(sock_fd, request.c_str(), request.size(), 0);
+
+    size_t buf_pos = 0;
+    string data;
+    data.resize(512);
+
+    while (1)
+    {
+        int n_bytes = recv(sock_fd, &data[buf_pos], data.size() - buf_pos, 0);
+        if (n_bytes == 0)
+        {
+            // Server closed
+            // TODO: if data is not empty, cout the message then close
+            close(sock_fd);
+            return;
+        }
+        buf_pos += n_bytes;
+
+        // if data buffer is full
+        if (data.size() == buf_pos)
+        {
+            data.resize(512 + data.size());
+        }
+
+        // check if full request sent
+        size_t req_end_pos = data.find("\r\n\r\n");
+        if (req_end_pos != string::npos)
+        {
+            string wire(data, 0, req_end_pos + 4);
+            data = string(data, req_end_pos + 4, buf_pos - (req_end_pos + 4));
+            buf_pos = data.length();
+            data.resize(512);
+
+            cout << wire << endl;
+
+            // TODO: break out once it has received all the requests
+            break;
+        }
+    }
+
+    close(sock_fd);
 }
 
-void Client::process_error(int status, std::string function)
+void Client::process_error(int status, string function)
 {
     if (status == -1)
     {
