18d56ba4e12227229099607f6a16136cd0591b6a
Jason Yang <jasonyang96@ucla.edu>
2016-04-30 13:57:40 -0700

    test_client changes

--- test_client.sh
+++ test_client.sh
@@ -1,3 +1,23 @@
 #! /bin/bash
 
-./web-client http://localhost:4000/test/index.html
+case "$1" in
+"1")
+	echo "getting google.com page"
+	../web-client http://google.com/index.html
+	;;
+"2")
+	echo "getting one local host object"
+	../web-client http://localhost:4000/test/make_test
+	;;
+"3")
+	echo "getting two local host objects"
+	../web-client http://localhost:4000/test/make_test http://localhost:4000/test/test.h
+	;;
+"4")
+	echo "getting two local host objects and awechords.com"
+	../web-client http://localhost:4000/test/make_test http://localhost:4000/test/test.h http://awechords.com/index.html
+	;;
+*)
+	echo "not supported"
+	;;
+esac;
