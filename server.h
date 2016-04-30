351364db6d95e782cdd91e9ba40a3a2322df614c
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2016-04-30 14:23:53 -0700

    changed tabs to spaces in server.h

--- server.h
+++ server.h
@@ -13,10 +13,10 @@ public:
     void accept_connections();
 
 private:
-	static void process_request(int socket_fd);
-	static void send_200_resp(int fd, bool keep_alive, struct stat buf, std::string version);
-	static void send_400_resp(int fd, std::string version);
-	static void send_404_resp(int fd, std::string version);
+    static void process_request(int socket_fd);
+    static void send_200_resp(int fd, bool keep_alive, struct stat buf, std::string version);
+    static void send_400_resp(int fd, std::string version);
+    static void send_404_resp(int fd, std::string version);
     static void process_error(int status, std::string function);
     int m_sockfd;
 };
